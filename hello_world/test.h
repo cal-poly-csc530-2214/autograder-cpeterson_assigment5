baf33c44e6a3ab6cbe2f57d359f523022da95852
Chris4a4 <Chris4a4@gmail.com>
2021-05-10 10:49:27 -0700

    Add files via upload

---
+++ hello_world/test.h
@@ -0,0 +1,16 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <cstring>
+
+#include "vops.h"
+
+namespace ANONYMOUS{
+}
+namespace ANONYMOUS{
+extern void doubleSketch__Wrapper(int x);
+extern void doubleSketch__WrapperNospec(int x);
+extern void doubleSketch(int x);
+}
+
+#endif
